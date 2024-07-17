708b4068a4303b728fdd5e98b10240ea22e563ec
Dave J <29786887+daveswork@users.noreply.github.com>
2024-07-17 16:49:24 -0400

    All tests passl

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(records){
+    const foundRecords = records.find(games => {
+        return games.result === "W"
+    });
+    if(foundRecords){
+        return foundRecords.year;
+    } else {
+        return foundRecords;
+    }
+}
